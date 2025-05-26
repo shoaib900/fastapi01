7cf9fecc92db28d2b6b3419c5702fa0daf0bc931
Shoaib Raza <rshoaib376@gmail.com>
2025-05-26 09:17:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,23 @@ app = FastAPI()
 @app.get("/")
 def data():
     return {"message":"hello My class how are you? I am fine well "}
+
+@app.get("/api")
+def data2():
+    return [
+        {
+            "name":"Shoaib",
+            "age":28,
+            "class": "MS Artificial Intelligence"
+        },
+         {
+            "name":"atif",
+            "age":23,
+            "class": "BS Artificial Intelligence"
+        },
+         {
+            "name":"ashutosh",
+            "age":25,
+            "class": "BS Artificial Intelligence"
+        },
+    ]
